c6d4dda8802edb3317777fd940bedcdd15b14694
Githel Lynn Suico <githelsuico@fb.com>
2020-07-06 12:01:09 -0700

    Milestone 7

---
+++ ParseChat/ChatNavigationController.h
@@ -0,0 +1,17 @@
+//
+//  ChatNavigationController.h
+//  ParseChat
+//
+//  Created by Githel Lynn Suico on 7/6/20.
+//  Copyright © 2020 Githel Lynn Suico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatNavigationController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ParseChat/ChatNavigationController.m
@@ -0,0 +1,32 @@
+//
+//  ChatNavigationController.m
+//  ParseChat
+//
+//  Created by Githel Lynn Suico on 7/6/20.
+//  Copyright © 2020 Githel Lynn Suico. All rights reserved.
+//
+
+#import "ChatNavigationController.h"
+
+@interface ChatNavigationController ()
+
+@end
+
+@implementation ChatNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
