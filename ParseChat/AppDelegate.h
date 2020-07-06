9a28ae41220751d362f9d3b50f3d88e3a0059aa5
Githel Lynn Suico <githelsuico@fb.com>
2020-07-06 10:03:11 -0700

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Githel Lynn Suico on 7/6/20.
+//  Copyright © 2020 Githel Lynn Suico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ParseChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseChat
+//
+//  Created by Githel Lynn Suico on 7/6/20.
+//  Copyright © 2020 Githel Lynn Suico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
